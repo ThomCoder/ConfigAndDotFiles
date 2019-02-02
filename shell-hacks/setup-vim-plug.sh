30aac966c60bd57481acb399bb0bffd80149b3ac
Thomas Rienößl <t.rienoessl@gmail.com>
2019-02-02 12:45:33 +0100

    change script to setup new plugin manager
    Signed-off-by: Thomas Rienößl <t.rienoessl@gmail.com>

--- shell-hacks/setup-vim-plug.sh
+++ shell-hacks/setup-vim-plug.sh
@@ -6,10 +6,5 @@
 #	https://github.com/VundleVim/Vundle.vim
 
 cd $HOME
-mkdir ~/.vim/tmp
-mkdir ~/.vim/tmp/backup
-mkdir ~/.vim/tmp/undo
-mkdir ~/.vim/tmp/swap
-mkdir ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
